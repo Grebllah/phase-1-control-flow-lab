6f275302b438d0c7979f3ac6858a0582db2aef32
James Hallberg <Grebllah81788@gmail.com>
2023-06-06 15:50:00 -0400

    complete P1 control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  // 1) gives customers a free sample if the ride is less than or equal to 400 feet
+  //     2) charges 20 dollars for a distance between 400 and 2000 feet
+  //     3) charges 30 dollars for a distance over 2000 feet
+  //     4) does not allow rides over 2500 feet
+  if (ride <= 400) {
+    return "This one is on me!"
+  }
+  else if (ride <= 2000 && ride > 400) {
+    return "That will be twenty bucks."
+  }
+  else if (ride <= 2500 && ride > 2000) {
+    return "I will gladly take your thirty bucks."
+  }
+  else if (ride > 2500) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === "NYC") {
+    return "Ok, sounds good."
+  }
+  else {return "No go."}
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much.";
+      break;
+      case 'not as generous':
+      return "Thank you.";
+      break;
+      default:
+      return "Bye.";
+      break;
+  }
 }
